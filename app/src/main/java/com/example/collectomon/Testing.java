948eb0fd245a60dd4282ea86bd2c68c7fd18f1f9
IndyPC <chibixryuu@gmail.com>
2023-12-28 15:26:29 +1100

    Test

--- app/src/main/java/com/example/collectomon/Testing.java
+++ app/src/main/java/com/example/collectomon/Testing.java
@@ -1,4 +1,9 @@
 package com.example.collectomon;
 
 public class Testing {
+    //This is a test class
+    public Testing(){
+        //This is a test constructor
+        System.out.println("This is a test constructor");
+    }
 }
