869d565458d7841545f4267ad7f68e092cf40f44
IndyPC <chibixryuu@gmail.com>
2023-12-28 15:27:54 +1100

    Testing Removal

--- app/src/main/java/com/example/collectomon/Testing.java
+++
@@ -1,9 +0,0 @@
-package com.example.collectomon;
-
-public class Testing {
-    //This is a test class
-    public Testing(){
-        //This is a test constructor
-        System.out.println("This is a test constructor");
-    }
-}
